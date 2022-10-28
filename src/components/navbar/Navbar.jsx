107abde8f45aaab1c7320e93e3e008bf2a9c202d
Amit <amitrozen2015@gmail.com>
2022-10-28 11:45:11 +0300

    added navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,51 @@
 import './navbar.scss'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search...' />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className='icon' />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className='icon' />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon' />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <FormatListBulletedOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <img
+              src="https://res.cloudinary.com/dcbbqlssh/image/upload/v1662395562/mern-blog/joanna-nix-walkup-h2pnXHMz8YM-unsplash_t7xu0m.jpg"
+              alt=""
+              className='avatar'
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
